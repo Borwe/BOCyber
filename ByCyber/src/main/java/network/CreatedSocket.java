a16cab3e57f1e70ddda33ab13762d17735061eac
Borwe <brian.orwe@gmail.com>
2018-07-18 12:36:52 +0300

    Update

--- ByCyber/src/main/java/network/CreatedSocket.java
+++ ByCyber/src/main/java/network/CreatedSocket.java
@@ -1,12 +1,23 @@
 package network;
 
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class CreatedSocket {
 
 	public static void searchDevicesInNetwork() {
+		getBroadcastAddress().stream().forEach(s->{
+			System.out.println(s);
+		});
+	}
+	
+	private static List<String> getBroadcastAddress() {
+		List<String> broadCastIps=new ArrayList<>();
 		try {
 			Enumeration<NetworkInterface> networkInterfaces=
 					NetworkInterface.getNetworkInterfaces();
@@ -18,18 +29,21 @@ public class CreatedSocket {
 					continue;
 				}
 				
-				/*for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) 
+				for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) 
 			    {
 			        InetAddress broadcast = interfaceAddress.getBroadcast();
 			        if (broadcast == null)
 			            continue;
 
 			        // Do something with the address.
-			    }*/
+			        broadCastIps.add(broadcast. getHostAddress());
+			    }
 			}
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		return broadCastIps;
 	}
 }
